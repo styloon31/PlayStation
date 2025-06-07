260bc4b3e4b937a7f989d20e8059a33709f7e6b3
Divyansh Rana <divyanshr323@gmail.com>
2025-06-07 15:31:34 +0530

    smooth scroll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // src/pages/Home.jsx
 import React from "react";
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Section1";
 import About from "../components/About";
@@ -8,7 +9,7 @@ import Story from "../components/Story";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
-const Home = () => {
+const HomeContent = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
@@ -19,6 +20,14 @@ const Home = () => {
       <Contact />
       <Footer />
     </main>
+  );
+};
+
+const Home = () => {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, smooth: true }}>
+      <HomeContent />
+    </ReactLenis>
   );
 };
 
